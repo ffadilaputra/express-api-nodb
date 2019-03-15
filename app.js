949f117ef563ea5e055ea3cd6014433cfde27059
ffadilaputra <i.fadilaputra@gmail.com>
2019-03-15 08:10:54 +0700

    Codeship test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const routes = require('./routes/index.routes')
 
 const app = express()
 
-app.use(morgan('tiny'))
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(routes)
